c3acae028e67e4f66c4a012a1ac8b9cbd4abfebc
Dustin Andrew Summers <dustin.a.summers@gmail.com>
2019-07-24 00:21:26 -0400

    Modifying tests

--- TestFile2/TestFile2.java
+++ TestFile2/TestFile2.java
@@ -1,14 +1,14 @@
 class TestFile2 {
 
     interface PostMan{
-        void letterTime(String message);
+        void letterTime(Message message);
     }
 
     static class Messenger implements PostMan{
 
         @Override
-        public void letterTime(String message) {
-            System.out.println("Sending a message: " + message);
+        public void letterTime(Message message) {
+            System.out.println("Sending a message: " + message.getMessage());
         }
     }
 
@@ -18,6 +18,22 @@ class TestFile2 {
     }
 
     private static void sendMessage(Messenger messenger){
-        messenger.letterTime("I'm implementing interfaces!!");
+        messenger.letterTime(new Message("I'm implementing interfaces!!"));
     }
+}
+
+class Message{
+        public Message(String message){
+            this.message = message;
+        }
+
+        private String message;
+
+        public void setMessage(String newMessage){
+            this.message = newMessage;
+        }
+
+        public String getMessage(){
+            return this.message;
+        }
 }
